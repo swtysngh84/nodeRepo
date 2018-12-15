80e639fc94e207129520196ff273251b15d2b193
lcom128-two <sweetysingh8469@gmail.com>
2018-12-15 17:15:15 +0530

    last update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-mongoose.connect(process.evn.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 module.exports={mongoose}
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser=require('body-parser')
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 app=express();
-Const Port=process.env.PORT || 2000;
+const Port=process.env.PORT || 2000;
 app.use(bodyParser.json());  
 app.post('/todos',(req,res)=>{
 var todo=new Todo({
